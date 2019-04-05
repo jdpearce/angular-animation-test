8da15455fa6618a064b78101953d7ff0773f1b84
Jo Pearce <jo.pearce@netwealth.com>
2019-04-05 21:45:10 +0100

    feat(animation): add simple animation to the angular logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
+export enum RotationState {
+    Default = 'default',
+    Left = 'left',
+    Right = 'right'
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    animations: [
+        trigger('rotation', [
+            state(
+                RotationState.Default,
+                style({
+                    transform: 'rotate(0)'
+                })
+            ),
+            state(
+                RotationState.Right,
+                style({
+                    transform: 'rotate(90deg)'
+                })
+            ),
+            state(
+                RotationState.Left,
+                style({
+                    transform: 'rotate(-90deg)'
+                })
+            ),
+            transition(`* => ${RotationState.Left}`, [animate('0.1s')]),
+            transition(`* => ${RotationState.Right}`, [animate('0.1s')]),
+            transition(`* => ${RotationState.Default}`, [animate('0.1s')])
+        ])
+    ]
 })
 export class AppComponent {
-  title = 'animation-test';
+    title = 'animation-test';
+    state: RotationState = RotationState.Default;
+
+    rotateRight(): void {
+        this.state = RotationState.Right;
+    }
+    rotateLeft(): void {
+        this.state = RotationState.Left;
+    }
+
+    reset(): void {
+        this.state = RotationState.Default;
+    }
 }
